0ad9d5ebfb3af0073edd97d48e6943533e57f5f4
GrigoryKoltso <154324680+GrigoryKoltso@users.noreply.github.com>
2024-02-18 17:35:15 +0100

    Add files via upload

---
+++ Homework9/script.js
@@ -0,0 +1,68 @@
+// Создать кнопку и красный квадрат с размерами 200х200px. При клике на кнопку менять у квадрата цвет заднего фона на зеленый и уменьшать его до размеров 100х100px.
+
+// const redDiv = document.querySelector(".redDiv")
+// const btnGreen = document.querySelector(".btnGreen")
+// btnGreen.addEventListener("click", function(){
+//     redDiv.style.backgroundColor = "green"
+//     redDiv.style.width = "100px"
+//     redDiv.style.height = "100px"
+// })
+
+// Создать кнопку и розовый квадрат с размерами 200х200px. При клике на кнопку менять цвет на синий и выводить в консоль обновленный цвет квадрата.
+
+/* const pinkDiv = document.querySelector(".pinkDiv");
+const btnBlue = document.querySelector(".blueBtn");
+
+btnBlue.addEventListener("click", function(){
+    const color = pinkDiv.style.backgroundColor = "blue"
+    console.log(color);
+    
+
+}) */
+
+// Создать кнопку и квадрат с размерами 150х150px. При клике на кнопку увеличивать высоту и ширину квадрата на 20px.
+
+// const bigDiv = document.querySelector(".bigDiv");
+// const bigButton = document.querySelector(".bigbutton");
+// bigButton.addEventListener("click", function (){
+//      bigDiv.style.width += "170px"
+//      bigDiv.style.height += "170px"
+// })
+
+
+// Создать кнопку и div с классом root. При клике на кнопку в div создается параграф синего цвета. Текст параграфа произвольный.
+/* const root = document.querySelector(".root");
+const blueButton = document.querySelector(".blueButton");
+blueButton.addEventListener("click", function(){
+const blue = root.innerHTML += `
+<p style="color: blue;">Kukusi)))</p>
+`
+}) */
+
+// Создать кнопку и div с классом root. При клике на кнопку в div создаются по очереди параграфы синего и зеленого цветов. Первый цвет синий.
+
+// const root = document.querySelector(".root");
+// const blueButton = document.querySelector(".blueButton");
+// blueButton.addEventListener("click", function(){
+// root.innerHTML += `<p style="color: blue;">Привет</p>`
+// root.innerHTML += `<p style="color: green;">Друг</p>`
+// })
+
+/* const root = document.querySelector(".root");
+const blueButton = document.querySelector(".blueButton");
+
+let clickCount = 0;
+
+blueButton.addEventListener("click", function(){
+    
+    if (clickCount % 2 === 0) {
+        root.innerHTML += '<p style="color: blue;">Привет</p>';
+    } else {
+        root.innerHTML += '<p style="color: green;">Друг</p>';
+    }
+    clickCount++
+}); */
+
+
+
+
